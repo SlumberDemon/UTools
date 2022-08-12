a305650716e131edd1e3225b27a840c8ab4292b3
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-08-12 23:18:24 +0200

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UTools
+
+# UTools
 Tools made with streamlit
+
+## Tools:  
+- Udownload | Download Videos
+- SleepyTube | Watch videos without restricitons
---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.markdown("# UTools")
+st.sidebar.markdown("# UTools")
+
+st.write("Use the side menu to select a tool to use!")
+st.markdown(
+    """## Tools: \n
+⬩ Udownload | Download videos \n
+⬩ SleepyTube | Watch videos without restricitons"""
+)
+
+st.caption("[Source](https://github.com/SlumberDemon/UTools)")
