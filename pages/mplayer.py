18f3fd770c2230f392c615e733a236e11deec5ea
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-08-13 13:37:30 +0200

    elements

--- pages/mplayer.py
+++ pages/mplayer.py
@@ -1,12 +1,9 @@
 import streamlit as st
-from streamlit_elements import media, mui
+from streamlit_elements import media, mui, dashboard, elements
 
-media_url = st.text_input("Media url", value="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
-    mui.CardHeader(title="MPlayer", className="draggable")
-    with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
-
-        # This element is powered by ReactPlayer, it supports many more players other
-        # than YouTube. You can check it out there: https://github.com/cookpete/react-player#props
-            
-        media.Player(url=media_url, width="100%", height="100%", controls=True)
+with elements("mplayer"):
+    with dashboard.Grid(layout, draggableHandle=".draggable"):
+        with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
+            mui.CardHeader(title="MPlayer", className="draggable")
+            with mui.CardContent(sx={"flex": 1, "minHeight": 0}):
+                media.Player(url=media_url, width="100%", height="100%", controls=True)
