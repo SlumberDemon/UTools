2d81c9dae236aef573d9fd33154674583d7ebfdd
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-08-13 13:41:05 +0200

    added url bar

--- pages/mplayer.py
+++ pages/mplayer.py
@@ -5,6 +5,10 @@ layout = [
     dashboard.Item("media", 0, 2, 12, 4),
 ]
 
+media_url = st.text_input(
+    "Media URL", value="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+)
+
 with elements("mplayer"):
     with dashboard.Grid(layout, draggableHandle=".draggable"):
         with mui.Card(key="media", sx={"display": "flex", "flexDirection": "column"}):
